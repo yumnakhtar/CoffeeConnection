212d52c822db620b6dbd6c02e76bd3aa0d9bc567
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-06-03 19:01:00 -0700

    add comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,13 @@
-// Your apiRoutes.js file should contain two routes:
 var queue = require("../data/queue.js");
 var path = require("path");
 
-
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-
-
 module.exports = function(app) {
+    //displays JSON of all possible friends
     app.get("/api/friends", function(req, res) {
         console.log(queue);
         res.json(queue);
     });
-
+    //check compatibility and add new friends
     app.post("/api/friends", function(req, res) {
         var newRes = req.body;
         queue.push(newRes);
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,11 @@
-// Your htmlRoutes.js file should include two routes:
 var path = require("path");
 
-
-// A GET Route to /survey which should display the survey page.
-// A default, catch-all route that leads to home.html which displays the home page.
-
 module.exports = function (app) {
-
+    //displays survey page
     app.get("/survey", function(req, res){
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-
+    //displays home page
     app.get("/", function(req,res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
