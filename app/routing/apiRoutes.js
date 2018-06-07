c21467ee6b854f6c727f796072c062fa76b04d97
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-06-06 18:32:26 -0700

    back end algorithm functional

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,10 +9,28 @@ module.exports = function(app) {
     });
     //check compatibility and add new friends
     app.post("/api/friends", function(req, res) {
+        //console.log(queue)
+        //console.log("this function is running")
         var newFriend = req.body;
+        var newFriendScores = newFriend.scores;
+
+        var leastDiff = 100;
+        //console.log(Math.abs(-5));
+        var buddy;
+       
+        for (var i=0; i<queue.length;i++){
+            var totalDif = 0;
+            for (var j=0;j<queue[i].scores.length; j++){
+                totalDif += Math.abs( parseInt(queue[i].scores[j]) - parseInt(newFriendScores[j]));
+            }
+           
+            if (totalDif < leastDiff){
+                leastDiff = totalDif;
+                buddy = queue[i];
+            }
+        }
         queue.push(newFriend);
-        res.json(newFriend);
+        res.json(buddy);
     });
 
 }
-
